796619f232f258859c2ae405dc2f871b983952a6
Robert <ostbat43@gmail.com>
2019-04-18 15:53:41 +0500

    screenshots

--- php/page.php
+++ php/page.php
@@ -61,7 +61,7 @@
 			<?php if ($page->coverImage()): ?>
 		   <div class="hover-img-base">
             <figure class="holy-sadie">
-             <a href="#" title="Your Post Title" class="hoverable-post wptg-link-post">
+             <a href="#" title="<?php echo $page->title(); ?>" class="hoverable-post wptg-link-post">
               <img class="img-fluid wptg-post-img" src="<?php echo $page->coverImage(); ?>" data-image="post" />
               <figcaption class="overlay-content">
                <div class="overlay-cape"></div>
